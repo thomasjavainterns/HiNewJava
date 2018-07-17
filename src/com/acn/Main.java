474fd64f9e3679c1494464f28b9656e06774e3ec
Tom du Plessis <ThomasD2@discovery.co.za>
2018-07-17 13:15:29 +0200

    Bla

--- src/com/acn/Main.java
+++ src/com/acn/Main.java
@@ -3,6 +3,6 @@ package com.acn;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello Java from MyNewBranch!");
     }
 }
